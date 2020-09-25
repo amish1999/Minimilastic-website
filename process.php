1ca8fed663e390a62712e1908b855347b3d55804
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2020-09-25 21:54:02 +0530

    process.php error to be fixed

--- process.php
+++ process.php
@@ -4,21 +4,12 @@
 
 	if(isset($_POST['submit'])) {
 		
-	$to = "edwin@codingfaculty.com";
-	$subject = $_POST['subject'];
-	$email = $_POST['email'];
-	$txt = $_POST['message'];
-	$headers = "From: " .$email . "\r\n" .
-	"CC: somebodyelse@example.com";
-	
-	mail($to,$subject,$txt,$headers);
-	
-	header("Location: contact.html");
-	
-
+       // Paste below 
 		
+		
+       // end 		
 	
-	}
+    }
 
 
 
